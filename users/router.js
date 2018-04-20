e0706274126ed481711e61735db19927897f3319
Ty Miller <ty.loves.code@gmail.com>
2018-04-20 09:39:25 -0400

    added comments

--- users/router.js
+++ users/router.js
@@ -10,6 +10,7 @@ const jsonParser = bodyParser.json();
 
 // Post to register a new user
 router.post('/', jsonParser, (req, res) => {
+  //Defines username and password
   const requiredFields = ['username', 'password'];
   const missingField = requiredFields.find(field => !(field in req.body));
 
@@ -22,6 +23,7 @@ router.post('/', jsonParser, (req, res) => {
     });
   }
 
+  //We check to make sure all the fields are strings
   const stringFields = ['username', 'password', 'firstName', 'lastName'];
   const nonStringField = stringFields.find(
     field => field in req.body && typeof req.body[field] !== 'string'
